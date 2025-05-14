2145bcff6f43cd6d2ad3e2a96e7da68fefe97957
freemanzok <ali.zokaei.1367@gmail.com>
2025-05-15 01:21:25 +0330

    fuck you

--- src/app/page.js
+++ src/app/page.js
@@ -6,15 +6,19 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/images/dashborad.jpg"
           alt="Next.js logo"
           width={180}
           height={38}
           priority
         />
-       hi hello 
+        hi hello
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <audio controls  src="/music/08 - Ken Tavr.mp3" >
+        
+        </audio>
+
+        {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -30,7 +34,7 @@ export default function Home() {
             />
             Deploy now
           </a>
-        </div>
+        </div> */}
       </main>
     </div>
   );
